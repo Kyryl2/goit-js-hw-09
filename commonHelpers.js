07be98d10fd7d28eb9ecbd9ed13b1d4bae8f612f
Kyryl2 <157823731+Kyryl2@users.noreply.github.com>
2024-04-02 18:38:19 +0000

    Deploying to gh-pages from @ Kyryl2/goit-js-hw-09@8ca5e53caf1d2c6de90c7433c68239b00b95ce82 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,2 +1,2 @@
-import"./assets/modulepreload-polyfill-3cfb730f.js";console.log("Gallery");
+import"./assets/modulepreload-polyfill-3cfb730f.js";import{S as l}from"./assets/vendor-870f0eb5.js";new l(".gallery a",{});
 //# sourceMappingURL=commonHelpers.js.map
