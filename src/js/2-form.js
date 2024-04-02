9c22f7cd0b54d973a743249de6c23b0e5a2afa2d
Kyryl2 <krasiuk.4884@gmail.com>
2024-04-02 23:04:35 +0200

    start2task

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -9,3 +9,20 @@ form.addEventListener('submit', clickButton);
 function clickButton(event) {
   event.preventDefault();
 }
+
+// const form = document.querySelector('.feedback-form');
+// const textarea = form.elements.message;
+// const localStorageKey = 'goit-example-message';
+
+// textarea.value = localStorage.getItem(localStorageKey) ?? '';
+
+// form.addEventListener('input', evt => {
+//   localStorage.setItem(localStorageKey, evt.target.value);
+// });
+
+// form.addEventListener('submit', evt => {
+//   evt.preventDefault();
+//   console.log(evt.target.elements.message.value);
+//   localStorage.removeItem(localStorageKey);
+//   form.reset();
+// });
