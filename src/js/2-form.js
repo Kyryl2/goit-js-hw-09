8801a43a8653e66194d71f884c648ab9d5f9b92a
Kyryl2 <krasiuk.4884@gmail.com>
2024-04-03 23:22:31 +0200

    task2 styles+js

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,28 +1,47 @@
 const form = document.querySelector('.feedback-form');
+const userData = {};
+
+function fillFeedbackFormFields() {
+  try {
+    const userDataFromLS = JSON.parse(
+      localStorage.getItem('feedback-form-state')
+    );
+
+    if (userDataFromLS === null) {
+      return;
+    }
+
+    // feedbackFormEl.elements.user_name.value = userDataFromLS.user_name;
+    // feedbackFormEl.elements.user_email.value = userDataFromLS.user_email;
+    // feedbackFormEl.elements.user_message.value = userDataFromLS.user_message;
+
+    for (const key in userDataFromLS) {
+      form.elements[key].value = userDataFromLS[key];
+    }
+  } catch (err) {
+    console.log(err);
+  }
+}
+fillFeedbackFormFields();
 
 form.addEventListener('input', setNewData);
-
-function setNewData(event) {}
-
 form.addEventListener('submit', clickButton);
+
+function setNewData(event) {
+  // const feedbackFieldEl = event.target;
+  const { target: feedbackFieldEl } = event;
+
+  const feedbackFieldName = feedbackFieldEl.name;
+  const feedbackFieldValue = feedbackFieldEl.value;
+
+  userData[feedbackFieldName] = feedbackFieldValue;
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(userData));
+}
 
 function clickButton(event) {
   event.preventDefault();
+  console.log(userData);
+  localStorage.removeItem('feedback-form-state');
+  form.reset();
 }
-
-// const form = document.querySelector('.feedback-form');
-// const textarea = form.elements.message;
-// const localStorageKey = 'goit-example-message';
-
-// textarea.value = localStorage.getItem(localStorageKey) ?? '';
-
-// form.addEventListener('input', evt => {
-//   localStorage.setItem(localStorageKey, evt.target.value);
-// });
-
-// form.addEventListener('submit', evt => {
-//   evt.preventDefault();
-//   console.log(evt.target.elements.message.value);
-//   localStorage.removeItem(localStorageKey);
-//   form.reset();
-// });
