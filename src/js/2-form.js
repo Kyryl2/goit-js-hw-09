43dae5ea4c558f05ba01098fdf3a31b69739d0aa
Kyryl2 <krasiuk.4884@gmail.com>
2024-04-04 17:23:42 +0200

    ok

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -49,13 +49,13 @@ function clickButton(event) {
   }
   if (!form.elements.email.value.trim()) {
     alert('Будь ласка, заповніть поле електронної пошти.');
-    console.log(formData);
+
     return;
   }
 
   if (!form.elements.message.value.trim()) {
     alert('Будь ласка, заповніть поле textarea.');
-    console.log(formData);
+
     return;
   } else {
     alert('Дякуємо за ваш відгук!');
@@ -65,41 +65,3 @@ function clickButton(event) {
     form.reset();
   }
 }
-
-// document.addEventListener('DOMContentLoaded', () => {
-//   const form = document.querySelector('.feedback-form');
-//   const emailInput = form.querySelector('input[name="email"]');
-//   const messageInput = form.querySelector('textarea[name="message"]');
-
-//   const formData =
-//     JSON.parse(localStorage.getItem('feedback-form-state')) || {};
-//   if (formData.email) {
-//     emailInput.value = formData.email;
-//   }
-//   if (formData.message) {
-//     messageInput.value = formData.message;
-//   }
-
-//   form.addEventListener('input', () => {
-//     const formData = {
-//       email: emailInput.value,
-//       message: messageInput.value,
-//     };
-//     localStorage.setItem('feedback-form-state', JSON.stringify(formData));
-//   });
-
-//   form.addEventListener('submit', event => {
-//     event.preventDefault();
-//     const formData =
-//       JSON.parse(localStorage.getItem('feedback-form-state')) || {};
-//     if (formData.email && formData.message) {
-//       console.log(formData);
-//       localStorage.removeItem('feedback-form-state');
-//       emailInput.value = '';
-//       messageInput.value = '';
-//       alert('Дякуємо за ваш відгук!');
-//     } else {
-//       alert('Будь ласка, заповніть поля електронної пошти та повідомлення.');
-//     }
-//   });
-// });
